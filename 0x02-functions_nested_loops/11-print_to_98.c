b3c044d979402aefa76c211a14c39036ccdd13ef
Motselisi <motselisileketa@gmail.com>
2022-07-05 10:35:03 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,30 +2,30 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - count the natural numbers from g to 98.
- * @g: value to count from.
+ * print_to_98 - count the natural numbers from n to 98.
+ * @n: value to count from.
  *
  */
-void print_to_98(int g)
+void print_to_98(int n)
 {
 int i;
 
-if (g == 98)
+if (n == 98)
 {
 printf("98");
 }
-else if (g < 98)
+else if (n < 98)
 {
-for (i = g ; i <= 98 ; i++)
+for (i = n ; i <= 98 ; i++)
 {
 printf("%d", i);
 if (i < 98)
 printf(", ");
 }
 }
-else if (g > 98)
+else if (n > 98)
 {
-for (i = g ; i >= 98 ; i--)
+for (i = n ; i >= 98 ; i--)
 {
 printf("%d", i);
 if (i > 98)
@@ -33,6 +33,6 @@ printf(", ");
 
 }
 }
-printf("\g");
+printf("\n");
 }
 
