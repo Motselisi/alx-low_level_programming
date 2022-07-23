6576434f77882a6be6a855e49ccb6d4edb9c740f
Motselisi <motselisileketa@gmail.com>
2022-07-23 08:34:57 -0700

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,18 @@
-#!/bin/bash
-gcc -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+#include <studio.h>
+#include "main.h"
+/**
+*main - is function entry
+*
+*@argc : a parameter
+*@argv : a parametr type char
+*return (0) : always success
+*/
+
+int main (in argc, char *argv[])
+{
+(void)argv;
+printf ("%d\", (argc - 1));
+
+return (0);
+}
 
