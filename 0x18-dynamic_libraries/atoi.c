d1dd1f55338b2ae1495766981ed09a805b18cd42
Motselisi <motselisileketa@gmail.com>
2022-10-03 12:05:25 -0700

    atoi.c

--- 0x18-dynamic_libraries/atoi.c
+++ 0x18-dynamic_libraries/atoi.c
@@ -9,9 +9,9 @@ int _atoi(char *s)
 int i = 1;
 unsigned int num = 0;
 do {
-(*s == '-')
+(*s == '-');
 i *= -1;
-else if (*s >= '0' && *s <= '9')
+if (*s >= '0' && *s <= '9')
 num = num * 10 + (*s - '0');
 else if (num > 0)
 break;
