310d75c87e86007bb35cf2430c0043c67bedcf1b
Motselisi <motselisileketa@gmail.com>
2022-10-21 09:00:32 -0700

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -7,11 +7,11 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-nsigned long hash = 5381;
-int c;
+  unsigned long hash = 5381;
+  int c;
 
-while ((c = *str++))
-hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+  while ((c = *str++))
+    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-return (hash);
+  return (hash);
 }
